2052b7d3d650e90914a6ba4d6991bd0d3e10be44
Henry Novoa <henry.r.novoa@gmail.com>
2023-05-06 01:56:08 +0200

    typescript boilerplate

---
+++ questions-improvements.md
@@ -0,0 +1,7 @@
+## Improvements
+
+Sometime the audio sounds very low
+
+## Questions
+
+If the tests worked without using the jest-ts-webcompat-resolver why are we installing it?
---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { hello } from './index';
+
+describe('Testing Jest Configuration', () => {
+  it('Const must have a HELLO value', () => {
+    expect(hello).toBe('HELLO CONFIGS');
+  });
+});
