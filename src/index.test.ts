10e5cb0fcb538ba8238ef4c19379b4fe50ef7d61
Henry Novoa <henry.r.novoa@gmail.com>
2023-05-06 02:01:13 +0200

    chages to gitignore and check if config works

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,6 @@ import { hello } from './index';
 
 describe('Testing Jest Configuration', () => {
   it('Const must have a HELLO value', () => {
-    expect(hello).toBe('HELLO CONFIGS');
+    expect(hello).toBe('HELLO CONFIGSS');
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export const hello = 'HELLO CONFIGS';
+export const hello = 'HELLO CONFIGSS';
